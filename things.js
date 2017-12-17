43733029be6dd16e2a539bf2eba6480ee1479913
PROPHESSOR <xenteze@gmail.com>
2017-12-17 19:33:55 +0200

    Добавлены все монстры Добавлены все вещи Добавлено всё оружие Добавлены все id

--- things.js
+++ things.js
@@ -1,111 +1,123 @@
 module.exports = {
     "1": 10557, //Axe
-    "2": 0, //FireExt?
+    "2": 10556, //FireExt?
     "3": 2001, //Shotgun
-    "4": 0, //SSG?
-    "5": 0, //Chaingun?
-    "6": 0, //Rocket Launcher
-    "7": 0, //Plasma Gun
-    "8": 0, //BFG?
-    "9": 0, //Pistol?
+    "4": 82, //SSG?
+    "5": 2002, //Chaingun?
+    "6": 2003, //Rocket Launcher
+    "7": 2004, //Plasma Gun
+    "8": 2006, //BFG?
+    "9": 5010, //Pistol?
 
-    "17": 0, //Zombie Pvt
-    "18": 0, // Zombie Lt
-    "19": 0, //Zombie Cpt
+    "17": 10274, //Zombie Pvt
+    "18": 10374, // Zombie Lt
+    "19": 10474, //Zombie Cpt
 
     "20": 10246, //Hellhound
     "21": 10346, //Cerberus
-    "22": 0, //Demon Wolf
+    "22": 10446, //Demon Wolf
 
-    "26": 0, //Impling
-    "27": 0, //Imp
-    "28": 0, //Imp-Lord
+    "23": 10264, //Troop
+    "24": 10364, //Commando
+    "25": 10464, //Assassin
 
-    "29": 0, //Phantom
-    "30": 0, //Lost Soul
-    "31": 0, //Nightmare //FIXME: Nightmare and Pain Elemental
+    "26": 10260, //Impling
+    "27": 10261, //Imp
+    "28": 10360, //Imp-Lord
+
+    "29": 10263, //Phantom
+    "30": 10463, //Lost Soul
+    "31": 10363, //Nightmare //FIXME: Nightmare and Pain Elemental
 
     "32": 10259, //Bull Demon
-    "33": 0, //Pinky
-    "34": 0, //Belphegor
+    "33": 10459, //Pinky
+    "34": 10359, //Belphegor
 
-    "35": 0, //Malrath
-    "36": 0, //Cacodemon
-    "37": 0, //Wretched?
+    "35": 10262, //Malrath
+    "36": 10362, //Cacodemon
+    "37": 10462, //Wretched
 
-    "44": 0, //Behemoth?
-    "45": 0, //Mancubus
-    "46": 0, //Druj
+    "38": 71, //Beholder TODO:
+    "39": 71, //Rahovart
+    "40": 71, //Pain Elemental
 
-    "47": 0, //Infernis
-    "48": 0, //Arch Vile
-    "49": 0, //Apollyon
+    "41": 10277, //Ghoul
+    "42": 10377, //Fiend
+    "43": 10477, //Revenant
 
-    "50": 0, //Ogre
-    "51": 0, //Hell Knight
-    "52": 0, //Baron of Hell
+    "44": 10278, //Behemoth?
+    "45": 10378, //Mancubus
+    "46": 10478, //Druj
 
-    "53": 0, //Cyberdemon?
+    "47": 10273, //Infernis
+    "48": 10373, //Arch Vile
+    "49": 10473, //Apollyon
 
-    "54": 0, //Kronos?
+    "50": 69, //Ogre TODO:
+    "51": 69, //Hell Knight
+    "52": 3003, //Baron of Hell
 
-    "62": 0, //Explosion? TODO:
+    "53": 16, //Cyberdemon?
 
-    "65": 0, //Red Key
-    "66": 0, //Blue Key?
-    "67": 0, //Green Key?
-    "68": 0, //Yellow Key?
+    "54": 10603, //Kronos?
+
+    "62": 10603, //Explosion? TODO:
+
+    "65": 86, //Red Key
+    "66": 85, //Blue Key?
+    "67": 10622, //Green Key?
+    "68": 87, //Yellow Key?
 
     "81": 10610, //Halon Can
-    "82": 0, //Halon Cans
+    "82": 10610, //Halon Cans TODO:
 
     "83": 2007, //Bullet Clip
-    "84": 0, //Bullet Clips
+    "84": 2048, //Bullet Clips TODO:
 
     "85": 2008, //Shell Clip
-    "86": 0, //Shell Clips
+    "86": 2049, //Shell Clips TODO:
 
-    "87": 0, //Rocket
-    "88": 0, //Rockets
+    "87": 2010, //Rocket
+    "88": 2046, //Rockets TODO:
 
-    "89": 0, //Cell Clip
-    "90": 0, //Cell Clips
+    "89": 2047, //Cell Clip
+    "90": 17, //Cell Clips TODO:
 
     "91": 2014, //Health Vial
     "92": 2015, //Armor Shard
 
     "93": 2018, //Armor
-    "94": 0, //Mega Armor
+    "94": 2019, //Mega Armor
 
     "95": 10620, //Credit
     "96": 10621, //Golden credit
 
-    "99": 0, //Sm Medkit
-    "100": 0, //Lg Medkit
+    "99": 10600, //Sm Medkit
+    "100": 10601, //Lg Medkit
     "101": 2013, //Soul Sphere
-    "102": 0, //Berserker
-    "110": 0, //Dog Collar
+    "102": 10623, //Berserker
+    "110": 10624, //Dog Collar
 
-    "128": 0, //Lava
+    "128": 10251, //Lava
     "129": 10250, //Fire
     "130": 10701, //Barrel
-    "132": 0, //Sink
-    "133": 0, //Toilet
+    "132": 10603, //Sink TODO:
+    "133": 10603, //Toilet TODO:
     "135": 2028, //Yellow Lamp
-    "138": 0, //EXIT
-    "139": 0, //Cabinet
-    "140": 0, //Blue Lamp
-    "141": 0, //Table
-    "142": 0, //Bed
+    "138": 10708, //EXIT
+    "139": 10707, //Cabinet
+    "140": 10703, //Blue Lamp
+    "141": 10702, //Table
+    "142": 10707, //Bed
 
-    "145": 0, //Civilian (Yellow)
-    "146": 0, //Scientist
-    "147": 0, //Dead Zombies FIXME: ZombieS FIXME: Green Marine???
-    "148": 0, //Black Scientist
-    "149": 0, //Dr. Guerard
-    "150": 0, //Purple Civilian
-    "151": 0, //Green Civilian
+    "145": 10510, //Civilian (Yellow)
+    "146": 10500, //Scientist
+    "147": 10520, //Dead Zombies FIXME: ZombieS FIXME: Green Marine???
+    "148": 10501, //Black Scientist
+    "149": 10502, //Dr. Guerard
+    "150": 10511, //Purple Civilian
+    "151": 10512, //Green Civilian
     //"161": 10705, //Yellow Civilian?
 
-    "162": 0, //Crate
+    "162": 10705, //Crate
 }
