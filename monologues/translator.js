ce0bbd4725f2f423502ebbe71a9f917ae6b63434
PROPHESSOR <xenteze@gmail.com>
2018-03-04 21:01:22 +0200

    Обновлен translator.js
    /monologues/translator.js обновлен в соответствии с StringParser:/stringparser.js

--- monologues/translator.js
+++ monologues/translator.js
@@ -1,20 +1,29 @@
-const fs = require('fs');//Библиотека для работы с файлами
+/* Copyright (c) 2017 DRRP Team
+ * 
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ * 
+ * Данная утилита позволяет преобразовать строки из сконвертированного (.txt) формата в скриптовый (.acs).
+ * Используйте эту программу после stringparser.cpp
+ */
+
+var fs = require('fs');//Библиотека для работы с файлами
 
 if(process.argv.length !== 4) return console.error("Использовать так: <источник> <выходной файл>");
-const fileFrom = process.argv[2]; //От куда читать
-const fileTo = process.argv[3];//Куда писать
+var fileFrom = process.argv[2]; //От куда читать
+var fileTo = process.argv[3];//Куда писать
 
-const file = fs.readFileSync(fileFrom); //Читаем файл
+var file = fs.readFileSync(fileFrom); //Читаем файл
 
-const replics = file.toString().split("\n\n"); //Разбиваем по репликам
-let output = ""; //Выходная переменная
-for(const i in replics){
-    let tmp = replics[i].replace(/-\n/g,'');
-    tmp = tmp.replace(/\n/g,' ');
-    output += `\ncase ${i}:`;
-    output += `\n\tprint(s:"${tmp}");`;
-    output += "\nbreak;\n";
+var replics = file.toString().split("\n\n"); //Разбиваем по репликам
+var output = ""; //Выходная переменная
+for(var i in replics){
+    var tmp = replics[i];
+    tmp = tmp.replace(/\n/g,'\\n'); //Заменяем фактические переносы строк на теоретические
+    output += "\ncase "+i+":";
+    output += '\n\tACS_NamedExecute("window", "'+tmp+'");'; //Используем скрипт окна для отображения текста
+    output += "\n\tbreak;\n";
 }
 
 fs.writeFileSync(fileTo, output);// Записываем выхлоп в файл
-console.log(output); //И выводим на экран
+//console.log(output); //И выводим на экран
