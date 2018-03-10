3a74d07e51c39d4fa5c1ea1b223f9bd4453340b5
UsernameAK <sitemakers.3dn.ru@gmail.com>
2018-03-10 12:39:42 +0200

    Бег отключён.

--- DECORATE.C
+++ DECORATE.C
@@ -34,6 +34,7 @@ Actor DoomRPGPlayer : DoomPlayer {
 	//Player.WeaponSlot 6, DRRPPlasmagun
 	//Player.WeaponSlot 7, DRRPBFG9000
 	Player.WeaponSlot 8, HellHoundGun, CerberusGun, DemonWolfGun
+	Player.RunHealth 245654
 	Health 30
 	Player.MaxHealth 30
 	Player.DisplayName "Marine"
