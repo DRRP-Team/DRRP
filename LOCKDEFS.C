3120da3682d6031e9358faeeda91a8ae92b2929f
PROPHESSOR <xenteze@gmail.com>
2018-07-22 16:13:53 +0300

    Глобальное наведение порядка и стандартификация
    * У DECORATE файлов расширения изменены на .dec
    * У ACS файлов расширения изменены на .acs
    * Изменены имена файлов
    ├ WEAPONS.JAVA -> DRRPWeapons.dec
    ├ SPARKS.JAVA -> Environment.dec
    └ Weap2.java -> Weapons.dec
    * Единый стиль DECORATE файлов
    * "1" в конце конфликтующих акторов заменены на "DRRP" в начале
    * Базовая логика Spark вынесена в абстрактный класс AbstractSpark
    * Нормальное название шейдера шлема (Helmet.fp)
    * Из LOCKDEFS убраны символы ^M, так же изменена сортировка
    * Убраны символы ^M из SNDINFO
    * Прочие мелкие форматирования кода
    
    Попутно исправлены баги:
    - Zombie Lt дропал шотган
    - Исправлен цвет Assasin'а
    - Civil>i<an

--- LOCKDEFS.C
+++ LOCKDEFS.C
@@ -1,33 +1,29 @@
 ClearLocks
 
-Lock 7
-{
-    GreenCard
-    Message "You need a green card to open this door"
-    RemoteMessage "You need a green card to activate this object"
-    Mapcolor 0 255 0
-}
-
-Lock 1
-{
-    RedCard1
+Lock 1 {
+    DRRPRedCard
     Message "You need a red card to open this door"
     RemoteMessage "You need a red card to activate this object"
     Mapcolor 255 0 0
 }
 
-Lock 3
-{
-    YellowCard1
+Lock 2 {
+    DRRPBlueCard
+    Message "You need a blue card to open this door"
+    RemoteMessage "You need a blue card to activate this object"
+    Mapcolor 0 0 255
+}
+
+Lock 3 {
+    DRRPYellowCard
     Message "You need a yellow card to open this door"
     RemoteMessage "You need a yellow card to activate this object"
     Mapcolor 255 255 0
 }
 
-Lock 2
-{
-    BlueCard1
-    Message "You need a blue card to open this door"
-    RemoteMessage "You need a blue card to activate this object"
-    Mapcolor 0 0 255
+Lock 7 {
+    DRRPGreenCard
+    Message "You need a green card to open this door"
+    RemoteMessage "You need a green card to activate this object"
+    Mapcolor 0 255 0
 }
