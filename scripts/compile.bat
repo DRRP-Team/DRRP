182808818fcf4d17e768b22184872fb34eee07c9
PROPHESSOR <xenteze@gmail.com>
2018-10-07 01:28:25 +0300

    Удалил папку scripts
    Эти скрипты всеравно никто не использовал

--- scripts/compile.bat
+++
@@ -1,6 +0,0 @@
-rem Copyright (c) 2017-2018 DRRP-Team
-rem 
-rem This software is released under the MIT License.
-rem https://opensource.org/licenses/MIT
-
-REM Здесь будет скрипт для компиляции проекта в PK3 и (-WAD-) файлы
