68d091479544d2a320bf35ebb313ebe9234dc678
PROPHESSOR <xenteze@gmail.com>
2018-11-05 21:14:08 +0200

    Локализованы сообщения дверей, требующих ключ
    Карточка: Сообщения дверей, требующих ключ не переведены

--- LOCKDEFS.C
+++ LOCKDEFS.C
@@ -9,28 +9,28 @@ ClearLocks
 
 Lock 1 {
     DRRPRedCard
-    Message "You need a red card to open this door"
+    Message "$DRRP_D_REDDOOR"
     RemoteMessage "You need a red card to activate this object"
     Mapcolor 255 0 0
 }
 
 Lock 2 {
     DRRPBlueCard
-    Message "You need a blue card to open this door"
+    Message "$DRRP_D_BLUEDOOR"
     RemoteMessage "You need a blue card to activate this object"
     Mapcolor 0 0 255
 }
 
 Lock 3 {
     DRRPYellowCard
-    Message "You need a yellow card to open this door"
+    Message "$DRRP_D_YELLOWDOOR"
     RemoteMessage "You need a yellow card to activate this object"
     Mapcolor 255 255 0
 }
 
 Lock 7 {
     DRRPGreenCard
-    Message "You need a green card to open this door"
+    Message "$DRRP_D_GREENDOOR"
     RemoteMessage "You need a green card to activate this object"
     Mapcolor 0 255 0
 }
