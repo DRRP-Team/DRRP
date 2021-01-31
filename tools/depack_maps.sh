de1e400344c88eef998cae648de544e4570b7b53
PROPHESSOR <xenteze@gmail.com>
2021-01-31 20:39:48 +0200

    Заставил скрипт распаковки карт работать из любого места вызова

--- tools/depack_maps.sh
+++ tools/depack_maps.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-for i in ../maps/*.wad
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+for i in $DIR/../maps/*.wad
 do
     echo
     echo Depacking $i
-    ./maptools.py depack $i
+    $DIR/maptools.py depack $i
     echo
     echo
 done
