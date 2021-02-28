a3635ed28d946375eefe8984aebcc4b911a6e8dd
PROPHESSOR <xenteze@gmail.com>
2021-03-01 01:25:08 +0200

    Подправлен скрипт сборки

--- tools/build.sh
+++ tools/build.sh
@@ -4,21 +4,21 @@ echo "Building production build of DRRP..."
 
 cd ../
 
-mkdir build
-mkdir build/maps
+rm -rf build
+mkdir -p build/maps
 
 cp -R maps/*.wad build/maps
-cp materials build/
-cp music build/
-cp patches build/
-cp shaders build/
-cp sounds build/
-cp sprites build/
-cp MapInfos build/
-cp graphics build/
-cp actors build/
-cp acs build/
-cp textures build/
-cp zscript build/
-cp ANIMDEFS build/
+cp -R materials build/
+cp -R music build/
+cp -R patches build/
+cp -R shaders build/
+cp -R sounds build/
+cp -R sprites build/
+cp -R MapInfos build/
+cp -R graphics build/
+cp -R actors build/
+cp -R acs build/
+cp -R textures build/
+cp -R zscript build/
+cp -R ANIMDEFS build/
 cp * build/
