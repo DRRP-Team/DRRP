5784f077754c2da91eee0cf7f84b3c13ba79bf0c
PROPHESSOR <xenteze@gmail.com>
2021-02-28 18:05:40 +0200

    Обновил CREDITS

--- TEMP/generate_credits.sh
+++ TEMP/generate_credits.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 TMP=$DIR/Credits.png
 OUTPUT=$DIR/../graphics/CREDITS.png
-HEIGHT=2700
+HEIGHT=3400
 
 chromium-browser --headless --hide-scrollbars --window-size=1008,$HEIGHT --screenshot="$TMP" "$DIR/Credits.html"
 
