578f88342d1a5cf1952e053b4afcc833402e941c
PROPHESSOR <xenteze@gmail.com>
2022-04-15 05:21:00 +0300

    Преобразовал большие буквы

--- filter/doom.id/fonts/transpile3.py
+++ filter/doom.id/fonts/transpile3.py
@@ -12,7 +12,7 @@ TO1 = 0x410
 files = []
 
 for id in range(FROM2 - FROM1 + 1):
-    FROM = f'0{hex(FROM1 + id)[2:]}.png'
+    FROM = f'00{hex(FROM1 + id)[2:]}.png'
     TO = f'0{hex(TO1 + id)[2:]}.png'
     print(f'{FROM} -> {TO}')
     os.system(f'mv defsmallfont/{FROM} defsmallfont/{TO}')
